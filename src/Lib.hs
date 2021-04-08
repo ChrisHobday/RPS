5ac2be272d6d0eb4379fe6ad4892fa629eaadf9c
Chris Hobday <hobday.chris@protonmail.com>
2021-04-08 12:21:05 -0400

    Implement countdown

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
     ( someFunc
+    , countdown
     ) where
+
+import Control.Concurrent
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+countdown :: IO ()
+countdown = do
+    putStrLn "3"
+    threadDelay 1000000
+    putStrLn "2"
+    threadDelay 1000000
+    putStrLn "1"
+    threadDelay 1000000
+    putStrLn "Shoot!"
+
+
+-- promptReady :: IO ()
+-- promptReady = do
+--     putStrLn "Ready?"
+--     _ <- getChar
