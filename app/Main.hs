f27e24cc081417fa719c576142ba3b4faeadfbde
Chris Hobday <hobday.chris@protonmail.com>
2021-04-08 12:29:37 -0400

    Implement starting ready prompt

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,7 @@ module Main where
 import Lib
 
 main :: IO ()
-main = countdown
+main = do
+  promptReady
+  _ <- getChar
+  countdown
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,25 +1,25 @@
 module Lib
-    ( someFunc
-    , countdown
-    ) where
+  ( promptReady
+  , countdown
+  ) where
 
 import Control.Concurrent
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+promptReady :: IO ()
+promptReady = do
+  putStrLn "Ready? (Enter)"
+
+getReady :: IO String
+getReady = _ <- getChar
 
 countdown :: IO ()
 countdown = do
-    putStrLn "3"
-    threadDelay 1000000
-    putStrLn "2"
-    threadDelay 1000000
-    putStrLn "1"
-    threadDelay 1000000
-    putStrLn "Shoot!"
+  putStrLn "3"
+  threadDelay 1000000
+  putStrLn "2"
+  threadDelay 1000000
+  putStrLn "1"
+  threadDelay 1000000
+  putStrLn "Shoot!"
 
-
--- promptReady :: IO ()
--- promptReady = do
---     putStrLn "Ready?"
---     _ <- getChar
