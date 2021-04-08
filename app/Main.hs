6f974efeb71d20774086088240bbfa2bc97f1577
Chris Hobday <hobday.chris@protonmail.com>
2021-04-08 12:46:33 -0400

    Add no buffering for input. Implement Sign datatype for RPS signs

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,18 @@
 module Main where
 
+import System.IO
+  ( hSetBuffering
+  , stdin
+  , BufferMode (NoBuffering) )
+
 import Lib
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering -- get Char without requiring enter
   promptReady
   _ <- getChar
   countdown
+  promptSign
+  sign <- getChar
+  putChar sign
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,17 +1,31 @@
 module Lib
   ( promptReady
   , countdown
+  , promptSign
   ) where
 
 import Control.Concurrent
 
+data Sign = 
+    Rock
+  | Paper
+  | Scissors
+  deriving
+    ( Eq 
+    , Enum
+    , Bounded )
+-- instance Show Suit where
+--   show Hearts   = "H"
+--   show Diamonds = "D"
+--   show Clubs    = "C"
+--   show Spades   = "S"
 
 promptReady :: IO ()
 promptReady = do
   putStrLn "Ready? (Enter)"
 
-getReady :: IO String
-getReady = _ <- getChar
+-- getReady :: IO String
+-- getReady = _ <- getChar
 
 countdown :: IO ()
 countdown = do
@@ -23,3 +37,9 @@ countdown = do
   threadDelay 1000000
   putStrLn "Shoot!"
 
+promptSign :: IO ()
+promptSign = do
+  putStrLn "(R)ock (P)aper (S)cissors"
+
+-- getSign :: IO Sign
+-- getSign = sign <- getChar
