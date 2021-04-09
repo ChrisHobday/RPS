121008cbcd53a1f110bf9d20a2ace05905d430df
Chris Hobday <hobday.chris@protonmail.com>
2021-04-09 15:53:53 -0400

    Add more comments

--- app/Main.hs
+++ app/Main.hs
@@ -16,18 +16,18 @@ import Lib
 
 main :: IO ()
 main = do
-  hSetBuffering stdin NoBuffering -- Get Char without requiring enter
-  promptReady
-  _ <- getChar
-  countdown
-  promptSign
-  charSign <- getChar
+  hSetBuffering stdin NoBuffering -- Get char without requiring enter
+  promptReady -- Prompt the player if they are ready
+  _ <- getChar -- Get any char from player to proceed
+  countdown -- Show a 3,2,1 countdown
+  promptSign -- Prompt the player for a sign
+  charSign <- getChar -- Get player sign as a char
   putStrLn "" -- Put empty line after charChar
-  aiSign <- aiChooseSign
+  aiSign <- aiChooseSign -- Generate random sign for ai
   let eitherPlayerSign = charToSign charSign
-  case eitherPlayerSign of -- Check eitherPlayerSign for either Error (player entered invalid sign) or Sign value
-    Left errorMessage ->
-      putStrLn errorMessage
-    Right playerSign -> do
-      showChoices playerSign aiSign
-      evaluateChoices playerSign aiSign
+  case eitherPlayerSign of -- Check eitherPlayerSign for either error (player entered invalid sign) or sign value
+    Left errorMessage -> -- If player entered invalid sign
+      putStrLn errorMessage -- Show error message for invalid sign
+    Right playerSign -> do -- If player entered valid sign
+      showChoices playerSign aiSign -- Show the sign choices of player and ai
+      evaluateChoices playerSign aiSign -- Evaluate and show the results of player and ai sign choices
--- src/Lib.hs
+++ src/Lib.hs
@@ -16,6 +16,7 @@ import Control.Concurrent
 import System.Random
   ( randomRIO )
 
+-- The different signs of a rock paper scissors game
 data Sign = 
     Rock
   | Paper
@@ -25,7 +26,7 @@ data Sign =
     , Eq
     , Bounded
     , Enum )
-instance Ord Sign where
+instance Ord Sign where -- Define the ordering of the different RPS signs (what beats what)
   compare Rock Paper     = LT
   compare Rock Scissors  = GT
   compare Paper Scissors = LT
@@ -33,10 +34,12 @@ instance Ord Sign where
   compare Scissors Rock  = LT
   compare Scissors Paper = GT
 
+-- Prompts the player if they are ready
 promptReady :: IO ()
 promptReady = do
   putStrLn "Ready? (Enter)"
 
+-- Shows a 3,2,1 countdown with delay
 countdown :: IO ()
 countdown = do
   putStrLn "3"
@@ -47,10 +50,12 @@ countdown = do
   threadDelay 1000000
   putStrLn "Shoot!"
 
+-- Prompts the user for a sign
 promptSign :: IO ()
 promptSign = do
   putStrLn "(R)ock (P)aper (S)cissors"
 
+-- Converts a char to a sign
 charToSign :: Char -> Either String Sign
 charToSign char
   | char == 'r' = Right Rock
@@ -61,14 +66,17 @@ charToSign char
   | char == 'S' = Right Scissors
   | otherwise   = Left "You entered an invalid sign, please choose (R)ock, (P)aper or (S)cissors next time."
 
+-- Generates a random sign
 aiChooseSign :: IO Sign
 aiChooseSign = do
   n <- randomRIO (0,2)
   return $ toEnum n
 
+-- Shows the player and ai sign choices (assumed player sign is first)
 showChoices :: Sign -> Sign -> IO ()
 showChoices s s' = putStrLn $ "You chose " ++ show s ++ " and your opponent chose " ++ show s'
 
+-- Evaluates and shows the results of player and ai sign choices (assumed player sign is first)
 evaluateChoices :: Sign -> Sign -> IO ()
 evaluateChoices s s'
   | s == s' = putStrLn "It's a tie!"
