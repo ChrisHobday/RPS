2c3e68f464e57be38271f8113907db2d7ed9987c
Chris Hobday <hobday.chris@protonmail.com>
2021-04-14 21:25:43 -0400

    Testing concurrency with tVars

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,37 @@
 import Lib
 
 import Data.Maybe
+import Control.Concurrent
 import Control.Concurrent.STM
-
-tF :: String
-tF = undefined
+import System.Random
 
 main :: IO ()
 main = do
   t <- newTVarIO Rock
   -- _ <- forkIO $ aiChooseSign >>= putMVar m
-  atomically $ writeTVar t Paper
+
+  _ <- forkIO $ do
+    aiSign <- aiChooseSign
+    putStrLn $ "Generated sign " ++ show aiSign
+    atomically $ writeTVar t aiSign
+    test <- readTVarIO t
+    print test
+
+  putStrLn "outside"
+
   test <- readTVarIO t
-  print $ test
+  print test
+
+  test <- readTVarIO t
+  print test
+
+  test <- readTVarIO t
+  print test
+
+  test <- readTVarIO t
+  print test
+
+  test <- readTVarIO t
+  print test
 
   return ()
